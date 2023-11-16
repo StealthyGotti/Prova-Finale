743f5767bc99d205ee9b0eba64aecaf8272f10b5
StealthyGotti <150790901+StealthyGotti@users.noreply.github.com>
2023-11-16 03:18:26 +0100

    Update ProvaFinale.m
    bestemmie e caffè

--- ProvaFinale.m
+++ ProvaFinale.m
@@ -30,10 +30,6 @@ addPlotOrbit(kepElf, mu, 1)
                         kepElf(3), kepElf(4), kepEli(6));
 fprintf( '\n dv_1: %.4f \n dt_1: %.2f\n' , dv_1 , dt_1)
 
-% Attesa fino al punto di manovra, cambio di anomalia del pericentro
-[dv_2, thf, dt_2] = changePerArg ( kepEli(1), kepEli(2), wi_1 , kepElf(5) , thf);
-fprintf( '\n dv_2: %.4f \n dt_2: %.2f\n' , dv_2 , dt_2)
-
 % Attesa fino al punto di manovra, cambio di forma dell'orbita
 dt_23 = flightTime( kepEli(1) , kepEli(2) , thf , 180);
 fprintf( '\n dt_23: %.2f\n' , dt_23)         % tempo per arrivare all'apogeo
@@ -43,30 +39,53 @@ fprintf( '\n dt_23: %.2f\n' , dt_23)         % tempo per arrivare all'apogeo
 dv_3 = dvi + dvf;
 fprintf( '\n dv_3: %.4f \n dt_3: %.2f\n' , dv_3 , dt_3)
 
-% Manovra biellittica
-raf = kepElf(1)*(1+kepElf(2));
-dv_vect = [];
-dt_3b_vect = [];
-rb = 1*raf ;
+% % Manovra biellittica
+% rpf = kepElf(1)*(1-kepElf(2));
+% dv_vect = [];
+% dt_3b_vect = [];
+% rb = 8000:.01:9000;
+% 
+% for i = 1 : length(rb)
+%     [dv1, dv2, dv3, thf, dt_3b] = biElliptic (kepEli(1) , kepEli(2), ...
+%                                 kepElf(1) , kepElf(2), rb(i), 180);
+%     dv_vect = [ dv_vect ; dv1+dv2+dv3];
+%     dt_3b_vect = [ dt_3b_vect ; dt_3b ];
+% end
+% rt = 6378;
+% 
+% figure
+% plot( rb , dt_3b_vect )
+% xlabel ('rb')
+% ylabel( 'dt')
+% 
+% dv_vant = 0;
+% for i = 1 : length(dv_vect)
+%     if (dv_vect(i) < dv_3) && (rb(i) > rt+100)
+%         % disp('Vantaggioso e raggio ammissibile ')
+%         if dv_vant == 0
+%             dv_vant = dv_vect(i);
+%             rb_vant = rb(i);
+%         elseif dv_vect(i) < dv_vant
+%             dv_vant = dv_vect(i);
+%             rb_vant = rb(i);
+%             i_vant = i;
+%         end
+%     end
+% end
+% 
+% figure
+% plot( rb , dv_vect )
+% xlabel ('rb')
+% ylabel( 'dv')
+% yline( dv_3 , '--k')
+% xline ( rt+100 , '--k')
+% yline( dv_vant , '--r')
+% % Biellittica dv di poco vantaggioso per certi valori di rb, ma svantaggiosa 
+% % per il tempo (differenza di circa 6154s)
 
-for i = 1 : length(rb)
-    [dv1, dv2, dv3, thf, dt_3b] = biElliptic (kepEli(1) , kepEli(2), ...
-                                kepElf(1) , kepElf(2), rb(i), 180);
-    dv_vect = [ dv_vect ; dv1+dv2+dv3];
-    dt_3b_vect = [ dt_3b_vect ; dt_3b ];
-end
-figure
-plot( rb , dv_vect )
-xlabel ('rb')
-ylabel( 'dv')
-figure
-plot( rb , dt_3b_vect )
-xlabel ('rb')
-ylabel( 'dt')
+% Attesa fino al punto di arrivo 
+dt_34 = flightTime( kepElf(1) , kepElf(2) , thf , kepElf(6));
+fprintf( '\n dt_34: %.2f\n' , dt_34)
 
-for i = 1 : length(dv_vect)
-    if dv_vect(i) <= dv_3
-        disp('Vantaggioso')
-    end
-end
+
 
