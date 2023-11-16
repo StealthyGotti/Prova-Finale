4343c71626e00fa1db05ce36469285ea99d0d680
CarloF19 <150793812+CarloF19@users.noreply.github.com>
2023-11-16 05:47:23 +0100

    Prova finale ripulita
    Script di prova finale ripulito (c'è ancora tanto lavoro, sì) ma con approccio numerazione e notazione che poi vi dico

--- ProvaFinale.m
+++ ProvaFinale.m
@@ -7,12 +7,14 @@ r0 = [-8205.9652; -4682.7991; 2382.4314];
 v0 = [1.8330; -5.5360; -2.2630];
 [kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), kepEli(6)] = car2kep(r0, v0, mu);
 fprintf('Parametri kepleriani orbita iniziale: [%.2f %.4f %.2f %.2f %.2f %.2f]\n', kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), kepEli(6))
-justPlotOrbit(kepEli, mu, 1)
+justPlotOrbit(kepEli, mu, .1)
 
 % Orbita finale
 kepElf = [11860.0000, 0.2614, rad2deg(0.5352), rad2deg(0.8005), rad2deg(2.3120), rad2deg(0.9719)];
 fprintf('Parametri kepleriani orbita finale: [%.2f %.4f %.2f %.2f %.2f %.2f]\n', kepElf(1), kepElf(2), kepElf(3), kepElf(4), kepElf(5), kepElf(6))
-addPlotOrbit(kepElf, mu, 1)
+addPlotOrbit(kepElf, mu, kepElf(6)+360, .1)
+[rf, ~] = kep2car(kepElf(1), kepElf(2), kepElf(3), kepElf(4), kepElf(5), kepElf(6), mu);
+plot3(rf(1), rf(2), rf(3), 'x', 'MarkerSize', 15, 'MarkerEdgeColor', 'r', 'LineWidth', 3)
 
 % PROBLEMI PIÙ GROSSI ATTUALMENTE:
 % 
@@ -24,68 +26,26 @@ addPlotOrbit(kepElf, mu, 1)
 %% Manovra standard
 % Posizione iniziale - caratterizzata 
 % Attesa fino al punto di manovra, seguito da un cambio di piano
-% Più si è distanti dal fuoco e meno costa --> punto di manovra == apocentro
+% Più si è distanti dal fuoco e meno costa --> punto di manovra == apocentro (no, scherzone)
 
-[dv_1, wi_1, thf, dt_1] = changeOrbPlane (kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), ...
-                        kepElf(3), kepElf(4), kepEli(6));
-fprintf( '\n dv_1: %.4f \n dt_1: %.2f\n' , dv_1 , dt_1)
+% Attesa fino al punto di manovra, cambio piano
+[dv1, w1, th1, dt1] = changeOrbPlane (kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), kepElf(3), kepElf(4), kepEli(6));
+addPlotOrbit(kepEli, mu, th1, .01)
 
-% Attesa fino al punto di manovra, cambio di forma dell'orbita
-dt_23 = flightTime( kepEli(1) , kepEli(2) , thf , 180);
-fprintf( '\n dt_23: %.2f\n' , dt_23)         % tempo per arrivare all'apogeo
+% Attesa fino al punto di manovra, cambio di anomalia del pericentro
+[dv2, th2, th3, dt2] = changePerArg ( kepEli(1), kepEli(2), w1 , kepElf(5) , th1);
+addPlotOrbit([kepEli(1) kepEli(2) kepElf(3) kepElf(4) w1 th1], mu, th2, .01)
 
-% Manovra bitangente
-[dvi, dvf, thf, dt_3] = biTangent (kepEli(1) , kepEli(2), kepElf(1) , kepElf(2), 180);
-dv_3 = dvi + dvf;
-fprintf( '\n dv_3: %.4f \n dt_3: %.2f\n' , dv_3 , dt_3)
+% Attesa fino al punto di manovra di cambio di forma dell'orbita
+dt23 = flightTime( kepEli(1) , kepEli(2) , th3 , 180); % tempo per arrivare all'apogeo
+addPlotOrbit([kepEli(1) kepEli(2) kepElf(3) kepElf(4) kepElf(5) th3], mu, 180, .01)
 
-% % Manovra biellittica
-% rpf = kepElf(1)*(1-kepElf(2));
-% dv_vect = [];
-% dt_3b_vect = [];
-% rb = 8000:.01:9000;
-% 
-% for i = 1 : length(rb)
-%     [dv1, dv2, dv3, thf, dt_3b] = biElliptic (kepEli(1) , kepEli(2), ...
-%                                 kepElf(1) , kepElf(2), rb(i), 180);
-%     dv_vect = [ dv_vect ; dv1+dv2+dv3];
-%     dt_3b_vect = [ dt_3b_vect ; dt_3b ];
-% end
-% rt = 6378;
-% 
-% figure
-% plot( rb , dt_3b_vect )
-% xlabel ('rb')
-% ylabel( 'dt')
-% 
-% dv_vant = 0;
-% for i = 1 : length(dv_vect)
-%     if (dv_vect(i) < dv_3) && (rb(i) > rt+100)
-%         % disp('Vantaggioso e raggio ammissibile ')
-%         if dv_vant == 0
-%             dv_vant = dv_vect(i);
-%             rb_vant = rb(i);
-%         elseif dv_vect(i) < dv_vant
-%             dv_vant = dv_vect(i);
-%             rb_vant = rb(i);
-%             i_vant = i;
-%         end
-%     end
-% end
-% 
-% figure
-% plot( rb , dv_vect )
-% xlabel ('rb')
-% ylabel( 'dv')
-% yline( dv_3 , '--k')
-% xline ( rt+100 , '--k')
-% yline( dv_vant , '--r')
-% % Biellittica dv di poco vantaggioso per certi valori di rb, ma svantaggiosa 
-% % per il tempo (differenza di circa 6154s)
+% Manovra bitangente all'apogeo, cambio di forma dell'orbita
+[dvi, dvf, thf, dt3, at, et] = biTangent (kepEli(1) , kepEli(2), kepElf(1) , kepElf(2), 180);
+dv3 = dvi + dvf;
+addPlotOrbit([at et kepElf(3) kepElf(4) kepElf(5) 180], mu, 0, .01)
 
-% Attesa fino al punto di arrivo 
-dt_34 = flightTime( kepElf(1) , kepElf(2) , thf , kepElf(6));
-fprintf( '\n dt_34: %.2f\n' , dt_34)
+% Attesa fino al punto finale
+addPlotOrbit([kepElf(1) kepElf(2) kepElf(3) kepElf(4) kepElf(5) 0], mu, kepElf(6), .01)
 
-
-
+title('Strategia standard')
