b11e18551532db580641518d5c49dfe273a0d9d3
CarloF19 <150793812+CarloF19@users.noreply.github.com>
2023-11-16 17:43:06 +0100

    Add files via upload

--- ProvaFinale.m
+++ ProvaFinale.m
@@ -7,85 +7,92 @@ r0 = [-8205.9652; -4682.7991; 2382.4314];
 v0 = [1.8330; -5.5360; -2.2630];
 [kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), kepEli(6)] = car2kep(r0, v0, mu);
 fprintf('Parametri kepleriani orbita iniziale: [%.2f %.4f %.2f %.2f %.2f %.2f]\n', kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), kepEli(6))
-justPlotOrbit(kepEli, mu, 1)
+figure
+hold on
+grid on
+xlabel('X [km]')
+ylabel('Y [km]')
+zlabel('Z [km]')
+set(gca, 'FontSize', 12)
+plotOrbit(kepEli, mu, .1)
+plot3(r0(1), r0(2), r0(3), 'o', 'MarkerSize', 10, 'MarkerEdgeColor', 'r', 'LineWidth', 3);
 
 % Orbita finale
 kepElf = [11860.0000, 0.2614, rad2deg(0.5352), rad2deg(0.8005), rad2deg(2.3120), rad2deg(0.9719)];
 fprintf('Parametri kepleriani orbita finale: [%.2f %.4f %.2f %.2f %.2f %.2f]\n', kepElf(1), kepElf(2), kepElf(3), kepElf(4), kepElf(5), kepElf(6))
-addPlotOrbit(kepElf, mu, 1)
+plotOrbit(kepElf, mu, .1)
+[rf, ~] = kep2car(kepElf(1), kepElf(2), kepElf(3), kepElf(4), kepElf(5), kepElf(6), mu);
+plot3(rf(1), rf(2), rf(3), 'x', 'MarkerSize', 15, 'MarkerEdgeColor', 'r', 'LineWidth', 3);
 
-% PROBLEMI PIÙ GROSSI ATTUALMENTE:
-% 
-% - gestire deg/rad ---------------------------------------------------- OK
-% - buttare giù lista di possibili sequenze di manovre
-%   (tutti i parametri cambiano, maremma caleidoscopica)
-% - sapere come consegnare codici Matlab®
+% Perigei
+[p1, ~] = kep2car(kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), 0, mu);
+plot3(p1(1), p1(2), p1(3),  'k.', 'MarkerSize', 30);
+[p2, ~] = kep2car(kepElf(1), kepElf(2), kepElf(3), kepElf(4), kepElf(5), 0, mu);
+plot3(p2(1), p2(2), p2(3), 'k.', 'MarkerSize', 30);
+
+legend('Orbita iniziale', 'Posizione iniziale', 'Orbita finale', 'Posizione finale', 'Perigei')
+title('Overview')
 
 %% Manovra standard
-% Posizione iniziale - caratterizzata 
-% Attesa fino al punto di manovra, seguito da un cambio di piano
-% Più si è distanti dal fuoco e meno costa --> punto di manovra == apocentro
 
-[dv_1, wi_1, thf, dt_1] = changeOrbPlane (kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), ...
-                        kepElf(3), kepElf(4), kepEli(6));
-fprintf( '\n dv_1: %.4f \n dt_1: %.2f\n' , dv_1 , dt_1)
+figure
+hold on
+grid on
+xlabel('X [km]')
+ylabel('Y [km]')
+zlabel('Z [km]')
+set(gca, 'FontSize', 12)
+plot3(r0(1), r0(2), r0(3), 'o', 'MarkerSize', 10, 'MarkerEdgeColor', 'r', 'LineWidth', 3)
+plot3(rf(1), rf(2), rf(3), 'x', 'MarkerSize', 15, 'MarkerEdgeColor', 'r', 'LineWidth', 3)
 
-% Attesa fino al punto di manovra, cambio di forma dell'orbita
-dt_23 = flightTime( kepEli(1) , kepEli(2) , thf , 180);
-fprintf( '\n dt_23: %.2f\n' , dt_23)         % tempo per arrivare all'apogeo
+% Attesa fino al punto di manovra, cambio piano
+[dv1, w1, th1, dt1] = changeOrbPlane (kepEli(1), kepEli(2), kepEli(3), kepEli(4), kepEli(5), kepElf(3), kepElf(4), kepEli(6));
+plotOrbitQuiver(kepEli, mu, 20, th1)
 
-% Manovra bitangente
-[dvi, dvf, thf, dt_3] = biTangent (kepEli(1) , kepEli(2), kepElf(1) , kepElf(2), 180);
-dv_3 = dvi + dvf;
-fprintf( '\n dv_3: %.4f \n dt_3: %.2f\n' , dv_3 , dt_3)
+% Attesa fino al punto di manovra, cambio di anomalia del pericentro
+[dv2, th2, th3, dt2] = changePerArg ( kepEli(1), kepEli(2), w1 , kepElf(5) , th1);
+plotOrbitQuiver([kepEli(1) kepEli(2) kepElf(3) kepElf(4) w1 th1], mu, 15, th2)
 
-% % Manovra biellittica
-% rpf = kepElf(1)*(1-kepElf(2));
-% dv_vect = [];
-% dt_3b_vect = [];
-% rb = 8000:.01:9000;
-% 
-% for i = 1 : length(rb)
-%     [dv1, dv2, dv3, thf, dt_3b] = biElliptic (kepEli(1) , kepEli(2), ...
-%                                 kepElf(1) , kepElf(2), rb(i), 180);
-%     dv_vect = [ dv_vect ; dv1+dv2+dv3];
-%     dt_3b_vect = [ dt_3b_vect ; dt_3b ];
-% end
-% rt = 6378;
-% 
+% Attesa fino al punto di manovra di cambio di forma dell'orbita
+dt23 = flightTime( kepEli(1) , kepEli(2) , th3 , 180); % tempo per arrivare all'apogeo
+plotOrbitQuiver([kepEli(1) kepEli(2) kepElf(3) kepElf(4) kepElf(5) th3], mu, 20, 180)
+
+% Manovra bitangente all'apogeo, cambio di forma dell'orbita
+[dvi, dvf, thf, dt3, at, et] = biTangent (kepEli(1) , kepEli(2), kepElf(1) , kepElf(2), 180);
+dv3 = dvi + dvf;
+plotOrbitQuiver([at et kepElf(3) kepElf(4) kepElf(5) 180], mu, 20, 0)
+
+% Attesa fino al punto finale
+plotOrbitQuiver([kepElf(1) kepElf(2) kepElf(3) kepElf(4) kepElf(5) 0], mu, 5, kepElf(6))
+
+% Tiro delle somme
+dvstd = dv1 + dv2 + dv3;
+fprintf('%cv complessivo procedura standard: %.2f km/s\n', 916, dvstd)
+dtstd = dt1 + dt2 + dt23 + dt3;
+fprintf('%ct complessivo manovra standard: %.0f s\n', 916, dtstd)
+
+% Tutte le manovre sono eseguite alla prima posizione utile
+
+[x, y] = meshgrid(-1.5*10000:50:1.5*10000, -10000*1.5:50:1.5*10000);
+z = zeros(size(x));
+surf(x, y, z, 'FaceAlpha', .75, 'EdgeColor', 'none', 'FaceColor', [.7 .7 .7]);
+Terra3d
+xlim([-1.5*10000 1.5*10000])
+ylim([-1.5*10000 1.5*10000])
+zlim([-1.5*10000 1.5*10000])
+
+legend('Init Pos', 'Fin Pos', 'Orb Init', 'Plane Changed', 'Periapsis Arg Changed', 'Shape Changed', 'Orb Fin')
+title('Strategia standard')
+
+%% Illustrazione cambio piano
+
 % figure
-% plot( rb , dt_3b_vect )
-% xlabel ('rb')
-% ylabel( 'dt')
-% 
-% dv_vant = 0;
-% for i = 1 : length(dv_vect)
-%     if (dv_vect(i) < dv_3) && (rb(i) > rt+100)
-%         % disp('Vantaggioso e raggio ammissibile ')
-%         if dv_vant == 0
-%             dv_vant = dv_vect(i);
-%             rb_vant = rb(i);
-%         elseif dv_vect(i) < dv_vant
-%             dv_vant = dv_vect(i);
-%             rb_vant = rb(i);
-%             i_vant = i;
-%         end
-%     end
-% end
-% 
-% figure
-% plot( rb , dv_vect )
-% xlabel ('rb')
-% ylabel( 'dv')
-% yline( dv_3 , '--k')
-% xline ( rt+100 , '--k')
-% yline( dv_vant , '--r')
-% % Biellittica dv di poco vantaggioso per certi valori di rb, ma svantaggiosa 
-% % per il tempo (differenza di circa 6154s)
-
-% Attesa fino al punto di arrivo 
-dt_34 = flightTime( kepElf(1) , kepElf(2) , thf , kepElf(6));
-fprintf( '\n dt_34: %.2f\n' , dt_34)
-
-
-
+% hold on
+% grid on
+% xlabel('X [km]')
+% ylabel('Y [km]')
+% zlabel('Z [km]')
+% set(gca, 'FontSize', 12)
+% plotOrbit(kepEli, mu, .01)
+% plotOrbit([kepEli(1) kepEli(2) kepElf(3) kepElf(4) w1 0], mu, .01)
+% title('Cambio piano')
