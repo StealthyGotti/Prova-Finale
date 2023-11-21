4720551c42b4731094a0165623fbf68fef4c80cf
CarloF19 <150793812+CarloF19@users.noreply.github.com>
2023-11-21 15:09:31 +0100

    nuova biTangent
    nuova biTangent per cambio forma con opzione per ribaltare vettore eccentricità (le chiamate precedenti funzionano perché se non si inserisce niente spegne il ribaltamento in automatico)

--- Funzioni/biTangent.m
+++ Funzioni/biTangent.m
@@ -1,19 +1,31 @@
-function [dvi, dvf, thf, dt, at, et] = biTangent (ai, ei, af, ef, thm)
+function [dvi, dvf, thf, dt, at, et] = biTangent (ai, ei, af, ef, thm, turnaround)
+
+if nargin == 5
+    turnaround = 'off';
+end
 
 thm = deg2rad(thm);
 
 mu = 398600;
 
-if thm == 0
+if thm == 0 && strcmp(turnaround, 'off')
     r1 = ai*(1-ei);
     r2 = af*(1+ef);
     thf = pi;
-elseif thm == pi
+elseif thm == pi && strcmp(turnaround, 'off')
     r1 = ai*(1+ei);
     r2 = af*(1-ef);
     thf = 0;
+elseif thm == 0 && strcmp(turnaround, 'on')
+    r1 = ai*(1-ei);
+    r2 = af*(1-ef);
+    thf = 0;
+elseif thm == pi && strcmp(turnaround, 'on')
+    r1 = ai*(1+ei);
+    r2 = af*(1+ef);
+    thf = pi;
 else
-    error('Only enter absidal points as maneuvering anomaly')
+    error('Only enter absidal points as maneuvering anomaly, or valid turnaround value: ''on'' or ''off''')
 end
 
 at = (r1+r2)/2;
