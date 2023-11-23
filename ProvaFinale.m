a2e33579ed9b5b40e31cf7cfffae3f4a40d5f963
StealthyGotti <150790901+StealthyGotti@users.noreply.github.com>
2023-11-23 16:41:41 +0100

    Update ProvaFinale.m
    Aggiunta la manovra 2 del simo (i valori continuano a tornare diversi da quelli che avevi scritto simo, nonostante abbia copia-incollato tutto quindi vi chiedo di dare un'occhiata perchè ho smadonnato ma non trovo errori di scambio variabili o altro).
    
    Ho committato direttamente nel main in quanto il codice principale non l'ho toccato. Ho inoltre provato a standardizzare la manovra due con grafici attinenti a quelli della manovra standard.
     
    Come attinenza secondo voi ci può stare?

--- ProvaFinale.m
+++ ProvaFinale.m
@@ -142,3 +142,98 @@ legend('Init Pos', 'Fin Pos', 'Orb Init', 'Plane Changed', 'Periapsis Arg Change
 title('Strategia standard')
 
 % ---------- DECIDERE STANDARD SCRITTURA MANOVRA --------------------------
+
+%% STRATEGIA 2 MAVORA Anomalia-Piano-Forma
+
+% cambio anomalia -> cambio piano (causante un cambio di w) -> cambio forma
+
+% ALLINEAMENTO ASSI ECCENTRICITA'
+
+a_i = kepEli(1);
+e_i = kepEli(2); 
+i_i = kepEli(3);
+OM_i = kepEli(4); 
+om_i = kepEli(5); 
+th_i = kepEli(6);
+
+a_f = kepElf(1);
+e_f = kepElf(2); 
+i_f = kepElf(3);
+OM_f = kepElf(4); 
+om_f = kepElf(5); 
+theta_f = kepElf(6);
+
+figure
+hold on
+grid on
+xlabel('X [km]')
+ylabel('Y [km]')
+zlabel('Z [km]')
+set(gca, 'FontSize', 12)
+plot3(r0(1), r0(2), r0(3), 'o', 'MarkerSize', 10, 'MarkerEdgeColor', 'r', 'LineWidth', 3)
+plot3(rf(1), rf(2), rf(3), 'x', 'MarkerSize', 15, 'MarkerEdgeColor', 'r', 'LineWidth', 3)
+
+om_st = om_f - (144.1217 - om_f); % anomalia del perigeo "strategico", il quale permette di ottenere
+...in seguito al cambio di piano (nel corrispettivo punto di manovra) l'anomalia di perigeo richiesta per l'orbita finale.
+
+[dv_1, thm ,thf1, dt_1 ] = changePerArg (a_i, e_i, om_i , om_st , th_i);
+%[delta_T] = timeOfFlight (a_i,e_i,th_ir,thm);
+
+plotOrbitQuiver([a_i , e_i , i_i , OM_i , om_i , th_i ], mu, 20, thm)  % tratto 1
+hold on
+
+% CAMBIO DI PIANO (INCLINAZIONE + ASCENSIONE RETTA DEL NODO ASCENDENTE)
+
+[dv_2, wf , thf2, dt2] = changeOrbPlane (a_i, e_i, i_i, OM_i, om_st, i_f, OM_f, thf1);
+
+plotOrbitQuiver([a_i , e_i , i_i , OM_i , om_st , thf1 ], mu, 5 , thf2)  % tratto 2
+hold on
+plotOrbitQuiver([a_i , e_i , i_f , OM_f , wf , thf2 ], mu, 2 , 180 )  % tratto 3
+
+% Tempo di trasferimento dal punto di cambio a piano (thf2) al punto di cambio forma (apogeo: 180°)
+dt3 = flightTime (a_i, e_i, thf2, 180);
+
+hold on
+
+% CAMBIO FORMA
+% 1) apocentro -> pericentro
+[ delta_v31 , delta_t41 , a_t1 , e_t1] = changeOrbitShape(a_i , e_i , wf , a_f , e_f , wf , 1);
+
+% 2) pericentro -> apocentro
+%[ delta_v32 , delta_t42 , a_t2 , e_t2] = changeOrbitShape(a_i , e_i , wf , a_f , e_f , wf , 2);
+% NB: la seconda opzione sul cambio di forma richiede un tempo di
+... trasferimento molto maggiore 
+    
+plotOrbitQuiver([a_t1 , e_t1 , i_f , OM_f , wf , 180 ], mu, 20, 0) % tratto 4  % da rivedere
+
+hold on
+
+plotOrbitQuiver([a_f , e_f , i_f , OM_f , wf , 0 ], mu, 5, theta_f) % tratto 5
+
+% Tempo di trasferimento dal perigeo della nuova orbita al punto d'arrivo (theta_f)
+dt5 = flightTime (a_f, e_f, 0 , theta_f);
+
+hold on
+
+Terra3d
+% 
+% plotOrbit(kepEli, mu, .1)
+% plot3(r0(1), r0(2), r0(3), 'o', 'MarkerSize', 10, 'MarkerEdgeColor', 'r', 'LineWidth', 3);
+% 
+% plotOrbit(kepElf, mu, .1)
+% plot3(rf(1), rf(2), rf(3), 'x', 'MarkerSize', 15, 'MarkerEdgeColor', 'r', 'LineWidth', 3);
+
+legend('Init Pos', 'Fin Pos', 'Orb Init', 'Periapsis Arg Changed','Plane Changed', 'Shape Changed', 'Orb Fin')
+title('Trasferimento APF')
+
+% COSTO TOTALE E TEMPO DI TRASFERIMENTO DELLA MISSIONE
+
+dv_TOT = abs(dv_1) + abs(dv_2) + abs(delta_v31);                            % 2.3491 km/s
+dt_TOT_sec = dt_1 + dt2 + dt3 + delta_t41 + dt5;                            % 10335 sec
+dt_TOT_ore = (dt_TOT_sec)/3600;                                             % 2.8706 ore ~= 2 ore e 52 minuti
+
+fprintf('%cv complessivo procedura Anomalia-Piano-Forma: %.2f km/s\n', 916, dv_TOT)
+fprintf('%cv complessivo procedura Anomalia-Piano-Forma: %.2f km/s\n', 916, dt_TOT_ore)
+
+
+
