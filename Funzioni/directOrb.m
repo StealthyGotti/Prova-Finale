ac96a78e25bfc273b7b72b0fe89a8db342b8e7e0
CarloF19 <150793812+CarloF19@users.noreply.github.com>
2023-11-26 04:03:27 +0100

    Aggiornamento directOrb
    Correzioni varie  e aggiunte warnings

--- Funzioni/directOrb.m
+++ Funzioni/directOrb.m
@@ -1,4 +1,4 @@
-function [dv, dt, kepElt, th2t, r1, r2, v1, vt1, vt2, v2, dt0, dt1, dt2] = directOrb (kepEli, kepElf, et, thm1, thm2)
+function [dv, dt, kepElt, th2t, dth, r1, r2, d1, d2, pt, v1, vt1, vt2, v2, dt0, dt1, dt2] = directOrb (kepEli, kepElf, et, thm1, thm2)
 
 mu = 398600;
 Rt = 6378;
@@ -49,7 +49,7 @@ d2 = norm(r2);
 i = r1/norm(r1);
 k = cross(r1,r2)/norm(cross(r1,r2));
 if dot(k,cross(r1,v1)) < 0 % setting coherent sense of rotation based on initial orbits
-    k = -k;
+    k = -k; % (specific angular momentum vector will be reversed)
 end
 j = cross(k,i);
 if dot(r2,j) >= 0
@@ -58,37 +58,48 @@ elseif dot(r2,j) < 0
     dth = 2*pi - acos(dot(i,r2)/norm(r2));
 end
 if et ~= 0 && d1 ~= d2
+    if et ~= 0 && d1 == d2
+        warning('Eccentricity could be set to 0 as selected maneuvering distances are equal to each other')
+    end
     syms th1t th2t pt real
     var = [th1t th2t pt];
     eqn = sym(zeros(3,1));
     eqn(1) = d1 == pt/(1+et*cos(th1t));
     eqn(2) = d2 == pt/(1+et*cos(th2t));
-    eqn(3) = dth == abs(th2t-th1t);
+    eqn(3) = dth == th2t-th1t;
     sol = vpasolve(eqn, var);
     th1t = double(sol.th1t);
-    th1t = mod(th1t, 2*pi);
-    if th1t < 0
-        th1t = th1t + 2*pi;
-    end
     th2t = double(sol.th2t);
-    th2t = mod(th2t, 2*pi);
-    if th2t < 0
-        th2t = th2t + 2*pi;
-    end
-    if isempty(th1t) || isempty(th2t)
+    pt = double(sol.pt);
+    if isempty(th1t) || isempty(th2t) || isempty(pt) % returning NaN values if no solution is found
         dv = NaN;
         dt = NaN;
         kepElt = NaN;
         th2t = NaN; 
+        dth = NaN; 
         r1 = NaN; 
         r2 = NaN; 
+        d1 = NaN; 
+        d2 = NaN; 
+        pt = NaN; 
         v1 = NaN; 
         vt1 = NaN; 
         vt2 = NaN; 
         v2 = NaN;
+        dt0 = NaN; 
+        dt1 = NaN; 
+        dt2 = NaN;
+        warning('Couldn''t find a solution for e = %.4f', et)
         return
     end
-    pt = double(sol.pt);
+    th1t = rem(th1t, 2*pi);
+    if th1t < 0
+        th1t = th1t + 2*pi;
+    end
+    th2t = rem(th2t, 2*pi);
+    if th2t < 0
+        th2t = th2t + 2*pi;
+    end
     ht = sqrt(pt*mu);
     ht = ht*k;
     I = [1; 0; 0];
@@ -111,10 +122,8 @@ if et ~= 0 && d1 ~= d2
     elseif et(3) < 0
         wt = 2*pi - acos((dot(et, Nt))/(norm(et)*norm(Nt)));
     end
-elseif et ~= 0 && d1 == d2
-    error('Eccentricity should be equal to 0 as selected maneuvering distances are equal to each other')
 elseif et == 0 && d1 ~= d2
-    error('Maneuvering distances should be equal to each other as selected eccentricity is equal to 0')
+    error('Maneuvering distances must be equal to each other as selected eccentricity is equal to 0')
 elseif et == 0 && d1 == d2
     pt = d1;
     ht = sqrt(pt*mu);
@@ -152,7 +161,7 @@ end
 et = norm(et);
 rpt = at*(1-et);
 if rpt < sh
-    warning('Possible Earth collision at e = %.4f', et)
+    warning('Possible Earth collision for e = %.4f', et)
 end
 kepElt = [at, et, rad2deg(it), rad2deg(Wt), rad2deg(wt), rad2deg(th1t)];
 
@@ -174,5 +183,6 @@ dt2 = flightTime(af, ef, rad2deg(thm2), rad2deg(thf));
 dv = dv1 + dv2;
 dt = dt0 + dt1 + dt2;
 th2t = rad2deg(th2t);
+dth = rad2deg(dth);
 
 end
